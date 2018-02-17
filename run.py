ad2a699dca1292df77e6c593272cfc1c42847e12
lov3catch <lov3catch@gmail.com>
2018-02-17 22:24:42 +0300

    debug

--- run.py
+++ run.py
@@ -1,8 +1,29 @@
 from flask import Flask, request
 from flask import jsonify
 
-# from handlers.zaycev_net.finder import parse_result, normalize_song_name, normalize_download_url
-from handlers.mail_ru.finder import parse_result, normalize_song_name, normalize_download_url
+from handlers.mail_ru.finder import normalize_download_url as mr_normalize_download_url
+from handlers.zaycev_net.finder import normalize_download_url as zn_normalize_download_url
+
+
+class DRIVERS:
+    ZAYCEV_NET = 'zaycev_net'
+    MAIL_RU = 'mail_ru'
+
+    @classmethod
+    def get_url_normalizer(cls, alias):
+        normalizers = {
+            cls.ZAYCEV_NET: zn_normalize_download_url,
+            cls.MAIL_RU: mr_normalize_download_url
+        }
+        return normalizers.get(alias)
+
+
+DEFAULT_DRIVER = DRIVERS.MAIL_RU
+
+if DEFAULT_DRIVER == DRIVERS.ZAYCEV_NET:
+    from handlers.zaycev_net.finder import parse_result, normalize_song_name
+else:
+    from handlers.mail_ru.finder import parse_result, normalize_song_name
 
 app = Flask(__name__)
 
@@ -28,14 +49,17 @@ def search():
 @app.route('/download')
 def download():
     download_url = request.args.get('url')
+    provider_alias = request.args.get('provider')
+
+    normalizer = DRIVERS.get_url_normalizer(provider_alias)
 
     result = {}
-    result['data'] = {'download_url': normalize_download_url(download_url)}
+    result['data'] = {'download_url': normalizer(download_url)}
 
     return jsonify(result)
 
 
 if __name__ == '__main__':
     app.run()
-# "/musicset/play/936394cc1b6ac6e01ea123f96033bd8a/3946953.json",
-# "http://zaycev.net/pages/39469/3946953.shtml?autoplay=1"
+    # "/musicset/play/936394cc1b6ac6e01ea123f96033bd8a/3946953.json",
+    # "http://zaycev.net/pages/39469/3946953.shtml?autoplay=1"
